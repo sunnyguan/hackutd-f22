15c34fc2abc58c20e32b271fd0ab44431fe41a68
Sunny Guan <sunny.peng.guan@gmail.com>
2022-11-13 03:10:34 -0600

    basic modal

--- frontend/pages/InvestmentChart.js
+++ frontend/pages/InvestmentChart.js
@@ -246,8 +246,35 @@ export default function InvestmentChart({id}) {
 
   return (
     <>
+      <div id="popup-modal" tabIndex="-1" style={{display: startYearModalOpen ? 'block' : 'none'}}
+           className="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 md:inset-0 h-modal md:h-full">
+        <div className="relative p-4 w-full max-w-md h-full md:h-auto m-auto" style={{
+          top: "50%",
+          transform: "translateY(-50%)"
+        }}>
+          <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
+            <button type="button"
+                    className="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
+                    data-modal-toggle="popup-modal"
+                    onClick={() => {setStartYearModalOpen(false)}}
+            >
+              <svg aria-hidden="true" className="w-5 h-5" fill="currentColor" viewBox="0 0 20 20"
+                   xmlns="http://www.w3.org/2000/svg">
+                <path fill-rule="evenodd"
+                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                      clip-rule="evenodd"></path>
+              </svg>
+              <span className="sr-only">Close modal</span>
+            </button>
+            <div className="p-6 text-center">
+              hello world
+            </div>
+          </div>
+        </div>
+      </div>
+
       <div className="flow-root px-4">
-        <p className="float-left space-x-4 mb-4">
+        <div className="float-left space-x-4 mb-4">
           <a>Start Year:</a>
           <button
             className={"bg-transparent transition-colors hover:bg-blue-500 hover:bg-opacity-50 text-white py-1 px-4 border border-blue-700 rounded"}
@@ -255,8 +282,8 @@ export default function InvestmentChart({id}) {
           >
             {startYear}
           </button>
-        </p>
-        <p className="float-right">
+        </div>
+        <div className="float-right">
           <div className="inline-flex space-x-1 bg-black rounded">
             <button
               className={
@@ -277,7 +304,7 @@ export default function InvestmentChart({id}) {
               Backtest
             </button>
           </div>
-        </p>
+        </div>
       </div>
       <div className={"flex"}>
         <div className={"flex-1"}>
