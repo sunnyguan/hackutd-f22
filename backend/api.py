a3b1c0ba6e2321f607c7f0b87279900c7cc552e7
Eric Zhang <36653830+ez314@users.noreply.github.com>
2022-11-13 00:22:44 -0600

    better asserts and assertion handling

--- backend/api.py
+++ backend/api.py
@@ -1,3 +1,6 @@
+import sys
+import traceback
+
 from flask import Flask, request, make_response
 from flask_cors import CORS
 
@@ -16,32 +19,52 @@ def root_():
 
 @app.route('/compute-monte-carlo', methods=['POST'])
 def compute_monte_carlo():
-    j = request.get_json()
-    investments = {
-        'cash': j['time_series']['cash'],
-        'stocks': j['time_series']['stocks'],
-        'bonds': j['time_series']['bonds'],
-    }
-    savings = j['time_series']['savings']
-    start_year = j['start_year']
-    num_sims = min(10000, max(100, j['num_sims']))
+    try:
+        j = request.get_json()
+        investments = {
+            'cash': j['time_series']['cash'],
+            'stocks': j['time_series']['stocks'],
+            'bonds': j['time_series']['bonds'],
+        }
+        savings = j['time_series']['savings']
+        start_year = j['start_year']
+        num_sims = min(10000, max(100, j['num_sims']))
 
-    monte_carlo_results = monte_carlo_sim(investments, savings, start_year, num_sims)
+        monte_carlo_results = monte_carlo_sim(investments, savings, start_year, num_sims)
+    except AssertionError as e:
+        _, _, tb = sys.exc_info()
+        tb_info = traceback.extract_tb(tb)
+        filename, line, func, text = tb_info[-1]
+        return make_response({
+            'text': text,
+            'loc': f"{filename}:{line}",
+            'tb': [f"{filename}:{line} - '{text}'" for filename, line, func, text in tb_info]
+        }, 422)
 
     return make_response(monte_carlo_results, 200)
 
 
 @app.route('/compute-backtest', methods=['POST'])
 def compute_backtest_():
-    j = request.get_json()
-    investments = {
-        'cash': j['time_series']['cash'],
-        'stocks': j['time_series']['stocks'],
-        'bonds': j['time_series']['bonds'],
-    }
-    savings = j['time_series']['savings']
-    start_year = j['start_year']
+    try:
+        j = request.get_json()
+        investments = {
+            'cash': j['time_series']['cash'],
+            'stocks': j['time_series']['stocks'],
+            'bonds': j['time_series']['bonds'],
+        }
+        savings = j['time_series']['savings']
+        start_year = j['start_year']
 
-    backtest_results = backtest_sim(investments, savings, start_year)
+        backtest_results = backtest_sim(investments, savings, start_year)
+    except AssertionError as e:
+        _, _, tb = sys.exc_info()
+        tb_info = traceback.extract_tb(tb)
+        filename, line, func, text = tb_info[-1]
+        return make_response({
+            'text': text,
+            'loc': f"{filename}:{line}",
+            'tb': [f"{filename}:{line} - '{text}'" for filename, line, func, text in tb_info]
+        }, 422)
 
     return make_response(backtest_results, 200)
