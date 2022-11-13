cc523a13495930af37b842059e82f0d15dd6fc96
lcnhi.ng@gmail.com <lcnhi.ng@gmail.com>
2022-11-13 10:17:20 -0600

    add landing page, sign up, log in

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -3,6 +3,7 @@ import Header from "./Header";
 import {useState} from "react";
 import SalaryChart from "./SalaryChart";
 import Dashboard from "./Dashboard";
+import LandingPage from "./LandingPage"
 
 export const headers = ["Home", "Budget", "Investments", "Salary"];
 
@@ -11,8 +12,10 @@ function Switch({tab}) {
     return <MovableChart />
   } else if (tab === "Salary") {
     return <SalaryChart />
-  } else if (tab === "Home") {
+  } else if (tab === "Budget") {
     return <Dashboard />
+  } else if (tab === "Home") {
+    return <LandingPage />
   }
 }
 
@@ -23,7 +26,7 @@ export default function Home() {
       <div style={{backgroundImage: "linear-gradient(180deg, #1F2833, #000000)", minHeight: '100vh', color: '#ffffff', fontSize: '25px'}}>
         <Header tab={tab} setTab={setTab} />
         <div className={"mx-9"}>
-          <h1 className={"text-5xl text-center m-10"} >
+          <h1 className={"text-5xl text-center m-10 font-bold"} >
             {tab}
           </h1>
           <Switch tab={tab} />
